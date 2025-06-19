af0242210e459b3a81e0895ae4b4544102e0a3e4
R2D2 <155892663+R2D2-08@users.noreply.github.com>
2025-06-19 23:50:01 +0530

    updates

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,12 +1,5 @@
 chmod 744 ${PWD}/scripts/installation.sh
 chmod 744 ${PWD}/scripts/containers.sh
 
-(
-./scripts/installation.sh
-)
-
-wait
-
-(
-./scripts/containers.sh
-)
+bash ${PWD}/scripts/installation.sh
+bash ${PWD}/scripts/containers.sh
