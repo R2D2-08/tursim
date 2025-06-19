a725377535c3dd700eb31e26f3d0af8883a3945e
R2D2 <155892663+R2D2-08@users.noreply.github.com>
2025-06-19 23:18:34 +0530

    remove scripts

--- containers.sh
+++
@@ -1,8 +0,0 @@
-echo "Spawning containers..."
-
-cp ${PWD}/backend/env.example ${PWD}/backend/.env
-cp ${PWD}/frontend/env.example ${PWD}/frontend/.env
-
-docker-compose up --build -d
-
-echo "Setup complete. Access Grafana on port 3000. The web-app should be accesible on the server's IP."
