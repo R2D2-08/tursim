6c490acfda2cb93f0ca9fbc5b4ae4e84182f2f8f
R2D2 <155892663+R2D2-08@users.noreply.github.com>
2025-06-19 22:38:13 +0530

    the other split

--- containers.sh
+++ containers.sh
@@ -1,1 +1,8 @@
-echo "Spawing containers..."
+echo "Spawning containers..."
+
+cp ${PWD}/backend/env.example ${PWD}/backend/.env
+cp ${PWD}/frontend/env.example ${PWD}/frontend/.env
+
+docker-compose up --build -d
+
+echo "Setup complete. Access Grafana on port 3000. The web-app should be accesible on the server's IP."
