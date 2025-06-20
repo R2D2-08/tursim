68396d57afd2d39cf3a5024fd6439cd95b6222bc
R2D2 <155892663+R2D2-08@users.noreply.github.com>
2025-06-20 09:40:41 +0530

    final api changes

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -9,7 +9,7 @@ import {
   ExecuteCodeResponse,
 } from '../types';
 
-const API_URL = process.env.API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 console.log('API Configuration:', {
   API_URL,
